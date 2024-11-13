b5729aa7f82a4c988aca6b90e80aac3104d9c81e
Денис <Den_1994_2015@mail.ru>
2024-11-13 08:50:13 +0300

    Time: 3 ms (61.85%) | Memory: 17.5 MB (10.24%) - LeetSync

---
+++ leetcode/557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        ans = ''
+        for i in s.split():
+            ans +=  i[::-1] + ' '
+            
+
+        return ans.strip()

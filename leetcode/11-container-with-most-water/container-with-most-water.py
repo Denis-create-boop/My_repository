918b433c8adf438e48fba32f6fb4963c345381f9
Денис <Den_1994_2015@mail.ru>
2024-11-18 10:10:31 +0300

    Time: 103 ms (44.09%) | Memory: 27.6 MB (86.10%) - LeetSync

--- leetcode/11-container-with-most-water/container-with-most-water.py
+++ leetcode/11-container-with-most-water/container-with-most-water.py
@@ -5,12 +5,12 @@ class Solution:
         ans = 0
 
         while left < right:
-            result = min(height[left], height[right]) * (right-left)
+            result = min(height[left], height[right]) * (right - left)
             ans = max(ans, result)
 
             if height[left] < height[right]:
                 left += 1
-
+            
             else:
                 right -= 1
         return ans

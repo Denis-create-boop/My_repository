861d4e162867e42cccddd8c5be069bae00c9def7
Денис <Den_1994_2015@mail.ru>
2024-11-18 10:31:08 +0300

    Time: 4 ms (64.96%) | Memory: 16.5 MB (83.41%) - LeetSync

--- leetcode/12-integer-to-roman/integer-to-roman.py
+++ leetcode/12-integer-to-roman/integer-to-roman.py
@@ -1,8 +1,8 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         rim = {
-            1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 
-            400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
+            1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C',
+            400: 'CD',  500: 'D', 900:'CM', 1000: 'M'
         }
         ans = ''
 
@@ -12,5 +12,4 @@ class Solution:
             while i <= num:
                 ans += rim[i]
                 num -= i
-
         return ans

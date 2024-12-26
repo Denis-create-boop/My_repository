4d50cf024f320f2213d01eee32de336cf2fc6e77
Денис <Den_1994_2015@mail.ru>
2024-12-26 14:17:57 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.05%) - LeetSync

---
+++ leetcode/74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        flag = False
+        for i in matrix:
+            if target in i:
+                flag = True
+                break
+
+        return flag

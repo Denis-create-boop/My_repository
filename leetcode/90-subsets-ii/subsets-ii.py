9701cedaa08f53461c76614825f1c3b5f17ee1f4
Денис <Den_1994_2015@mail.ru>
2025-01-12 15:35:19 +0300

    Time: 0 ms (100.00%) | Memory: 12.8 MB (10.97%) - LeetSync

---
+++ leetcode/90-subsets-ii/subsets-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subsetsWithDup(self, S):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        res = [[]]
+        S.sort()
+        for i in range(len(S)):
+            if i == 0 or S[i] != S[i - 1]:
+                l = len(res)
+            for j in range(len(res) - l, len(res)):
+                res.append(res[j] + [S[i]])
+        return res
